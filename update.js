524bf0d98f480dd87f41c34caaff39b02588c7a8
win59993 <57604409+win59993@users.noreply.github.com>
2019-11-10 23:10:44 +0000

    Create update.js

---
+++ update.js
@@ -0,0 +1,9 @@
+{
+	"versionCode":10,
+	"versionName":"1.0.0",
+	"fileSize":"35.93M",
+	"apkUrl":"http://www.lofter.com/rsc/android/lofter.apk",
+	"required":false,
+	"releaseDate":"2019-11-12 12:45:20",
+	"releaseNotes":["新版本特性新版本特性新版本特性；", "描述版本信息，方便用户选择是否立即下载更新；", "性能优化和BUG修复。"]
+}
